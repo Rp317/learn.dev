a33c434b877ec8a72c6cf0d576fafa12a6d88d92
Rp317 <purajarikrussell@gmail.com>
2022-03-21 14:20:33 -0500

    2 files modified

--- index.tsx
+++ index.tsx
@@ -10,13 +10,30 @@ class App extends Component<AppProps, AppState> {
   render() {
     return (
       <div id="page">
-        <div id="header"></div>
-        <div id="information"></div>
-        <div id="weFrickingCare"></div>
-        <div id="creator"></div>
-        <div id="joinNow"></div>
-        <div id="theBottom"></div>
+        <div id="header">
+          <h1>learn.dev</h1>
+          <button>info</button>
+          <button>sign up</button>
+          <button>log on</button>
+        </div>
+        <div id="information">
+          <h1>We are a business that teaches you how to <h1>learn/create/develop/push</h1></h1>
+          <h1>We offer a <strong>var</strong>iety of languages including</h1>
+          <div>javascript/python/typescript/node and many more</div><button></button>
+        </div>
+        <div id="weFrickingCare">We care for the customers as we give them 1 on 1 tutoring if they get stuck/need motovation</div>
+        <div id="creator">The creator is a minority child who like to make this company for the people, not the dollar.</div>
+        <div id="joinNow">
+          JOIN NOW FUTURE <h1>creator/developer/leader</h1>
+        </div>
+        <div id="theBottom">
+          <button>learn.dev</button>
+          <button>programs</button>
+          <button>pricing</button>
+          <button>list of items to learn</button>
+        </div>
       </div>
+     
     );
   }
 }
