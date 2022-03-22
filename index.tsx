ad21179ffc17601cdf002a185436243f3b1a39a8
Rp317 <purajarikrussell@gmail.com>
2022-03-22 12:11:47 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -17,14 +17,14 @@ class App extends Component<AppProps, AppState> {
           <button id="owo">log on</button>
         </div>
         <div id="information">
-          <h1>We are a business that teaches you how to <h1>learn/create/develop/push</h1></h1>
+          <h1>We are a business that teaches you how to <strong>learn/create/develop/push</strong></h1>
           <h1>We offer a <strong>var</strong>iety of languages including</h1>
-          <div>javascript/python/typescript/node and many more</div><button></button>
+          <strong>javascript/python/typescript/node and many more</strong><button></button>
         </div>
         <div id="weFrickingCare">We care for the customers as we give them 1 on 1 tutoring if they get stuck/need motovation</div>
         <div id="creator">The creator is a minority child who like to make this company for the people, not the dollar.</div>
         <div id="joinNow">
-          JOIN NOW FUTURE <h1>creator/developer/leader</h1>
+          JOIN NOW FUTURE <strong>creator/developer/leader</strong>
         </div>
         <div id="theBottom">
           <button>learn.dev</button>
